a5911265824f9afa82e00e23a584fa8fdeb6b893
Mikhail Bruskov <mvbruskov@avito.ru>
2020-05-16 13:35:07 +0300

    Nemca's version

--- circle-fifths/circle-of-fifths_test.go
+++ circle-fifths/circle-of-fifths_test.go
@@ -2,17 +2,53 @@ package main
 
 import "testing"
 
-// TestChordCircle function for test my app
-func TestChordCircle(t *testing.T) {
-	assertCorrectMessage := func(t *testing.T, got, want string) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+var assertCorrect = func(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
-	t.Run("circle of fifths", func(t *testing.T) {
-		got := ChordCircle(c)
-		want := "C chord have Am parallel chord"
-		assertCorrectMessage(t, got, want)
+}
+
+func TestGetMinor(t *testing.T) {
+	t.Run("C to Am", func(t *testing.T) {
+		want := "Am"
+		got, _ := getMinor("C")
+		assertCorrect(t, got, want)
+	})
+	t.Run("Unknown major error", func(t *testing.T) {
+		want := "Unknown chord"
+		_, err := getMinor("Z")
+		if err != nil {
+			assertCorrect(t, err.Error(), want)
+		}
+	})
+	t.Run("Blank major error", func(t *testing.T) {
+		want := "You didn't said chord"
+		_, err := getMinor("")
+		if err != nil {
+			assertCorrect(t, err.Error(), want)
+		}
+	})
+}
+
+func TestGetMajor(t *testing.T) {
+	t.Run("Am to C", func(t *testing.T) {
+		want := "C"
+		got, _ := getMajor("Am")
+		assertCorrect(t, got, want)
+	})
+	t.Run("Unknown minor error", func(t *testing.T) {
+		want := "Unknown chord"
+		_, err := getMajor("Zm")
+		if err != nil {
+			assertCorrect(t, err.Error(), want)
+		}
+	})
+	t.Run("Blank minor error", func(t *testing.T) {
+		want := "You didn't said chord"
+		_, err := getMajor("")
+		if err != nil {
+			assertCorrect(t, err.Error(), want)
+		}
 	})
 }
